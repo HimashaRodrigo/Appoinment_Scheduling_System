8644b57fbac4b9a57e21de65df41083e596429a3
HimashaRodrigo <himasharodrigo1998@gmail.com>
2023-08-26 22:39:04 +0530

    add filtering fro jobs

--- client/src/pages/Receptionist/Receptionist_Place_Appoinment.jsx
+++ client/src/pages/Receptionist/Receptionist_Place_Appoinment.jsx
@@ -8,19 +8,25 @@ const ReceptionistPlaceAppoinment = ({menuItems,backRoutes}) => {
     const {data:users, isPending:isPending2} = useFetch('api/v1/user/');
     const JobSeekers = users?.data?.Users[1];
     let filteredConsaltants = [];
+    let filteredjobs = [];
     users?.data?.Users[0].map((data)=>{
         if(data.Status !== "Deactive" && data.Role === "Consaltant"){
             filteredConsaltants.push(data);
         }
     })
+    jobs?.data?.jobCategories.map((data)=>{
+        if(data.Status !== "Not Available"){
+            filteredjobs.push(data);
+        }
+    })
     const Consaltant = filteredConsaltants;
-
+    
     return ( 
         <>
             <DashBoard menuItems={menuItems} backRoutes={backRoutes} rightContainer={
                 <>
                     {isPending1 && isPending2 && <Spinner/>}
-                    {users && jobs && <MakeAppoinment JobSeekers={JobSeekers} Consaltant={Consaltant} jobs={jobs?.data?.jobCategories}/>}
+                    {users && jobs && <MakeAppoinment JobSeekers={JobSeekers} Consaltant={Consaltant} jobs={filteredjobs}/>}
                 </>
             }/>
         </>
